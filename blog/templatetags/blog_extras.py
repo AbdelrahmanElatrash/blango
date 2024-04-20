3a7fe05f57af0190cccfae8ff797ae41e18d8bc8
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2024-04-20 20:32:46 +0000

    custom tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,11 +2,17 @@ from django.contrib.auth import get_user_model
 from django import template 
 from django.utils.html import format_html 
 from django.utils.safestring import mark_safe
+from blog.models import Post
 
 register = template.Library()
 
-@register.filter
-def author_details(author,current_user=None):
+@register.simple_tag(takes_context=True)
+def author_details(context):
+    
+    request = context['request']
+    current_user = request.user
+    post = context["post"]
+    author = post.author
     
     if not isinstance(author , get_user_model()):
         return ""
@@ -28,3 +34,29 @@ def author_details(author,current_user=None):
         suffix = ""
         
     return format_html('{} {} {}',prefix,name,suffix)
+
+
+@register.simple_tag()
+def row(extra_classes=""):
+    
+    return format_html('<div class="row" {}>', extra_classes)
+    
+    
+@register.simple_tag()
+def endrow():
+    
+    return format_html("</div>")
+
+@register.inclusion_tag('blog/post-list.html')
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk) [:5]
+    return {'title':"Recent Posts" , 'posts':posts}
+
+
+@register.simple_tag()
+def col(extra_classes=""):
+    return format_html('<div class="col" {}>', extra_classes)
+
+@register.simple_tag()
+def endcol():
+    return format_html("</div>")
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, post_detail
 
-# urlpatterns = [
-#     path('', index , name='home')
+urlpatterns = [
+    path('', index , name='home'),
+    path('post/<slug>', post_detail , name='blog-post-detail' ),
 ]
