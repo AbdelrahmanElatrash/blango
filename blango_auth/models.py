2aa14b46d3acdcbd5d3c4ed3a1bab865ab194624
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2024-05-02 15:58:03 +0000

    add custome user model

--- blango_auth/models.py
+++ blango_auth/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser , UserManager
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
 
 class BlangoUserManager(UserManager):
@@ -23,8 +25,25 @@ class BlangoUserManager(UserManager):
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
         
+        if extra_fields.get('is_stuff') is not True :
+            raise ValuError ('super user must have is_staff set to True')
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValuError ('super user mudt have is_superuser set to True')
+        
+        return self._create_user(email,password,**extra_fields)
+        
         
         
         pass 
 class User(AbstractUser):
-    pass 
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    
+    objects =BlangoUserManager()
+    
+    USERNAME_FIELD ="email"
+    REQUIRED_FIELDS =[]
+    
+    def __str__(self):
+        return self.email
