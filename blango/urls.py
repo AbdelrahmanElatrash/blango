15ed127f63d88a0dada171e73454efe36dcd7e1a
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2024-05-03 14:56:52 +0000

    rejestration varefication

--- blango/urls.py
+++ blango/urls.py
@@ -17,10 +17,17 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings 
 import debug_toolbar
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile,name="profile"),
+    path('accounts/register/', RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register"),
+    path("accounts/",include("django_registration.backends.activation.urls")),
     path('',include('blog.urls')),
 ]
 
